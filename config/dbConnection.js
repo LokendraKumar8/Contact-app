04b19a1de84db00f926b3500ed2a436741278039
LokendraKumar8 <lokendrakumar582809@gmail.com>
2025-09-20 19:49:04 +0530

    deployment ready

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -3,8 +3,8 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
     try {
       // console.log(process.env.CONNECTION_STRIN);
-      const connect = await mongoose.connect("mongodb+srv://lokendrakumar582809:Lokendra456@cluster0.oey3g.mongodb.net/mycontacts-backend?retryWrites=true&w=majority&appName=Cluster0");
-      console.log("Database connected:", connect.connection.host,connect.connection.name);
+      const connect = await mongoose.connect(process.env.URL);
+      console.log("Database connected:");
     } catch (error) {
       console.log(error);
       process.exit(1);
--- server.js
+++ server.js
@@ -3,17 +3,14 @@ const connectDB = require('./config/dbConnection');
 const errorHandler = require('./middleware/errorHandler');
 const dotenv = require('dotenv').config();
 
+
 connectDB();
 const app = express();
 const port =process.env.PORT || 5000;
 
-app.use(express.json());//we need a middleware to parse the json data from the client side
+app.use(express.json(URL));//we need a middleware to parse the json data from the client side
 app.use("/api/contacts", require("./routes/contactRoutes"));
 app.use("/api/users", require("./routes/userRoutes"));
-app.get("/heloo",(req,res)=>{
-    res.send("<h1>heloo</h1>")
-}
-)
 app.use(errorHandler);
 
 app.get('/', (req, res) => {
